c413665bc8987544d30ec7df7983ba57264ed57d
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-07-16 13:56:31 +0300

    feat(routes): `/cards/<slug>` returns information about card and it's scores

--- app/routes/cards.py
+++ app/routes/cards.py
@@ -46,6 +46,54 @@ def create_card():
     return redirect(url_for("cards.view_card", slug=slug))
 
 
-@bp.route("/<string:slug>")
+@bp.route("/<string:slug>", methods=["GET", "POST"])
 def view_card(slug: str):
-    return slug
+    if request.method == "GET":
+        db = get_db()
+
+        card_query = """
+        SELECT
+            cards.id AS card_id,
+            cards.slug as card_slug,
+            cards.card_name,
+            cards.created_at AS card_created,
+            scores.id AS score_id,
+            scores.player_name AS player_name,
+            scores.points AS points,
+            scores.created_at AS score_created
+        FROM
+            cards
+        LEFT JOIN
+            scores
+        ON
+            cards.id = scores.card_id
+        WHERE cards.slug = ?;
+        """
+
+        card_rows = db.execute(card_query, (slug,)).fetchall()
+
+        if not card_rows:
+            flash("Error: card not found")
+            return redirect(url_for("index.index"))
+
+        card = {
+            "id": card_rows[0]["card_id"],
+            "slug": card_rows[0]["card_slug"],
+            "card_name": card_rows[0]["card_name"],
+            "created_at": card_rows[0]["card_created"],
+        }
+
+        scores = [
+            {
+                "id": row["score_id"],
+                "player_name": row["player_name"],
+                "points": row["points"],
+                "created_at": row["score_created"],
+            }
+            for row in card_rows
+            if row["score_id"] is not None
+        ]
+
+        return render_template("view_card.html", card=card, scores=scores)
+
+    return "Not supported!"
